1f3ed01846be9350cc916f282ea7cfc78f7a13aa
CinoPlayz <90247617+CinoPlayz@users.noreply.github.com>
2025-05-05 23:59:53 +0200

    Added files for tiled and object for teleporting

--- core/src/main/java/io/github/some_example_name/igra/MapFactory.java
+++ core/src/main/java/io/github/some_example_name/igra/MapFactory.java
@@ -1,8 +1,12 @@
 package io.github.some_example_name.igra;
 
+import io.github.some_example_name.igra.maps.LabyrinthMap;
+
 public class MapFactory {
     public static BaseMap createMap(String mapPath, Player player) {
         //if (mapPath.endsWith("forest.tmx")) return new ForestMap(player);
+
+        if (mapPath.endsWith("labirint.tmx")) return new LabyrinthMap(player);
 
         return new DefaultMap(mapPath, player); // Fallback
     }
---
+++ core/src/main/java/io/github/some_example_name/igra/maps/LabyrinthMap.java
@@ -0,0 +1,15 @@
+package io.github.some_example_name.igra.maps;
+
+import io.github.some_example_name.igra.BaseMap;
+import io.github.some_example_name.igra.Player;
+
+public class LabyrinthMap extends BaseMap {
+    public LabyrinthMap(Player player) {
+        super("tiled/Labyrinth/Tiled_files/labirint.tmx", player);
+    }
+
+    @Override
+    protected void setupAdditionalLayers() {
+
+    }
+}
