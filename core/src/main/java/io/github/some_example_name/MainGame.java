c266c98675f449a61d0c77fa9080ef2bc2ca38c5
Laanmi <milan.momcilovic03@gmail.com>
2025-05-15 19:58:43 +0200

    intro video and main menu screen

--- core/src/main/java/io/github/some_example_name/MainGame.java
+++ core/src/main/java/io/github/some_example_name/MainGame.java
@@ -1,26 +1,26 @@
 package io.github.some_example_name;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
-import io.github.some_example_name.igra.*;
+import io.github.some_example_name.screens.IntroScreen;
 
-public class MainGame extends ApplicationAdapter {
-    private OrthographicCamera camera;
+/**
+ * Main game class that extends Game to support screens
+ * Converted from ApplicationAdapter to Game
+ */
+public class MainGame extends Game {
+    // Shared resources
     private SpriteBatch batch;
-    private TextureAtlas playerTexture;
-    private Sound damageSound, pickupSound;
     private BitmapFont font;
     private BitmapFont fontSmaller;
-    private Player player;
-    private BaseMap currentMap;
-    private EnemyManager enemyManager;
-    private TextureAtlas enemyTexture;
+
+    // Getters for shared resources
+    public SpriteBatch getBatch() {
+        return batch;
+    }
 
     public BitmapFont getFont() {
         return font;
@@ -32,58 +32,44 @@ public class MainGame extends ApplicationAdapter {
 
     @Override
     public void create() {
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, 1280, 720);
+        // Initialize shared resources
         batch = new SpriteBatch();
         font = new BitmapFont();
         fontSmaller = new BitmapFont(Gdx.files.internal("assets/lsans-small.fnt"),
             Gdx.files.internal("assets/lsans-small.png"), false);
 
-        playerTexture = new TextureAtlas("assets/player.txt");
-        enemyTexture = new TextureAtlas("assets/enemy.txt");
-        damageSound = Gdx.audio.newSound(Gdx.files.internal("tiled/hit.mp3"));
-        pickupSound = Gdx.audio.newSound(Gdx.files.internal("tiled/pickup.mp3"));
+        // Start with the intro screen
+        setScreen(new IntroScreen(this));
 
-        player = new Player(playerTexture, damageSound, pickupSound);
-        enemyManager = new EnemyManager(enemyTexture, damageSound, pickupSound);
-        currentMap = MapFactory.createMap("tiled/starting.tmx", player, enemyManager);
-        camera.zoom = currentMap.getDefaultZoom();
-        player.setMap(currentMap);
+        Gdx.app.log("MainGame", "Game initialized");
     }
 
     @Override
     public void render() {
-        currentMap.update(Gdx.graphics.getDeltaTime());
-
-        if (currentMap.shouldSwitchMap()) {
-            currentMap.dispose();
-            currentMap = MapFactory.createMap(currentMap.getNextMapPath(), player, enemyManager);
-            player.setMap(currentMap);
-            camera.zoom = currentMap.getDefaultZoom();
-        }
-
-        if (currentMap.shouldCameraTrackPlayer()) {
-            camera.position.set(player.getX() + 32, player.getY() + 32, 0);
-        }
-
-        camera.update();
-        if (currentMap.isSmallerScale()){
-            currentMap.render(camera, batch, fontSmaller);
-        } else {
-            currentMap.render(camera, batch, font);
-        }
-
+        // Let the screen system handle rendering
+        super.render();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        playerTexture.dispose();
-        damageSound.dispose();
-        pickupSound.dispose();
-        font.dispose();
-        fontSmaller.dispose();
-        currentMap.dispose();
-        enemyTexture.dispose();
+        // Dispose shared resources
+        if (batch != null) {
+            batch.dispose();
+        }
+
+        if (font != null) {
+            font.dispose();
+        }
+
+        if (fontSmaller != null) {
+            fontSmaller.dispose();
+        }
+
+        // Dispose current screen if exists
+        if (getScreen() != null) {
+            getScreen().dispose();
+        }
+
+        Gdx.app.log("MainGame", "Game disposed");
     }
 }
