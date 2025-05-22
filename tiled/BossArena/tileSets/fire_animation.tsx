dfb13240ebf186493944751649c641f38e4db587
DomenJakopic <domen.jakopic2@gmail.com>
2025-05-22 10:35:34 +0200

    boss arena map

---
+++ tiled/BossArena/tileSets/fire_animation.tsx
@@ -0,0 +1,244 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="fire_animation" tilewidth="16" tileheight="16" tilecount="198" columns="11">
+ <image source="../craftpix-dungeon/Tiled_files/fire_animation.png" width="176" height="288"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="150"/>
+   <frame tileid="33" duration="150"/>
+   <frame tileid="66" duration="150"/>
+   <frame tileid="99" duration="150"/>
+   <frame tileid="132" duration="150"/>
+   <frame tileid="165" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="150"/>
+   <frame tileid="34" duration="150"/>
+   <frame tileid="67" duration="150"/>
+   <frame tileid="100" duration="150"/>
+   <frame tileid="133" duration="150"/>
+   <frame tileid="166" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <animation>
+   <frame tileid="2" duration="150"/>
+   <frame tileid="35" duration="150"/>
+   <frame tileid="68" duration="150"/>
+   <frame tileid="101" duration="150"/>
+   <frame tileid="134" duration="150"/>
+   <frame tileid="167" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <animation>
+   <frame tileid="3" duration="150"/>
+   <frame tileid="36" duration="150"/>
+   <frame tileid="69" duration="150"/>
+   <frame tileid="102" duration="150"/>
+   <frame tileid="135" duration="150"/>
+   <frame tileid="168" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <animation>
+   <frame tileid="4" duration="150"/>
+   <frame tileid="37" duration="150"/>
+   <frame tileid="70" duration="150"/>
+   <frame tileid="103" duration="150"/>
+   <frame tileid="136" duration="150"/>
+   <frame tileid="169" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="5">
+  <animation>
+   <frame tileid="5" duration="150"/>
+   <frame tileid="38" duration="150"/>
+   <frame tileid="71" duration="150"/>
+   <frame tileid="104" duration="150"/>
+   <frame tileid="137" duration="150"/>
+   <frame tileid="170" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <animation>
+   <frame tileid="6" duration="150"/>
+   <frame tileid="39" duration="150"/>
+   <frame tileid="72" duration="150"/>
+   <frame tileid="105" duration="150"/>
+   <frame tileid="138" duration="150"/>
+   <frame tileid="171" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="7">
+  <animation>
+   <frame tileid="7" duration="150"/>
+   <frame tileid="40" duration="150"/>
+   <frame tileid="73" duration="150"/>
+   <frame tileid="106" duration="150"/>
+   <frame tileid="139" duration="150"/>
+   <frame tileid="172" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="11">
+  <animation>
+   <frame tileid="11" duration="150"/>
+   <frame tileid="44" duration="150"/>
+   <frame tileid="77" duration="150"/>
+   <frame tileid="110" duration="150"/>
+   <frame tileid="143" duration="150"/>
+   <frame tileid="176" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <animation>
+   <frame tileid="12" duration="150"/>
+   <frame tileid="45" duration="150"/>
+   <frame tileid="78" duration="150"/>
+   <frame tileid="111" duration="150"/>
+   <frame tileid="144" duration="150"/>
+   <frame tileid="177" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="13">
+  <animation>
+   <frame tileid="13" duration="150"/>
+   <frame tileid="46" duration="150"/>
+   <frame tileid="79" duration="150"/>
+   <frame tileid="112" duration="150"/>
+   <frame tileid="145" duration="150"/>
+   <frame tileid="178" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="14">
+  <animation>
+   <frame tileid="14" duration="150"/>
+   <frame tileid="47" duration="150"/>
+   <frame tileid="80" duration="150"/>
+   <frame tileid="113" duration="150"/>
+   <frame tileid="146" duration="150"/>
+   <frame tileid="179" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="15">
+  <animation>
+   <frame tileid="15" duration="150"/>
+   <frame tileid="48" duration="150"/>
+   <frame tileid="81" duration="150"/>
+   <frame tileid="114" duration="150"/>
+   <frame tileid="147" duration="150"/>
+   <frame tileid="180" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="16">
+  <animation>
+   <frame tileid="16" duration="150"/>
+   <frame tileid="49" duration="150"/>
+   <frame tileid="82" duration="150"/>
+   <frame tileid="115" duration="150"/>
+   <frame tileid="148" duration="150"/>
+   <frame tileid="181" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="17">
+  <animation>
+   <frame tileid="17" duration="150"/>
+   <frame tileid="50" duration="150"/>
+   <frame tileid="83" duration="150"/>
+   <frame tileid="116" duration="150"/>
+   <frame tileid="149" duration="150"/>
+   <frame tileid="182" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="18">
+  <animation>
+   <frame tileid="18" duration="150"/>
+   <frame tileid="51" duration="150"/>
+   <frame tileid="84" duration="150"/>
+   <frame tileid="117" duration="150"/>
+   <frame tileid="150" duration="150"/>
+   <frame tileid="183" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="22">
+  <animation>
+   <frame tileid="22" duration="150"/>
+   <frame tileid="55" duration="150"/>
+   <frame tileid="88" duration="150"/>
+   <frame tileid="121" duration="150"/>
+   <frame tileid="154" duration="150"/>
+   <frame tileid="187" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="23">
+  <animation>
+   <frame tileid="23" duration="150"/>
+   <frame tileid="56" duration="150"/>
+   <frame tileid="89" duration="150"/>
+   <frame tileid="122" duration="150"/>
+   <frame tileid="155" duration="150"/>
+   <frame tileid="188" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="24">
+  <animation>
+   <frame tileid="24" duration="150"/>
+   <frame tileid="57" duration="150"/>
+   <frame tileid="90" duration="150"/>
+   <frame tileid="123" duration="150"/>
+   <frame tileid="156" duration="150"/>
+   <frame tileid="189" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="25">
+  <animation>
+   <frame tileid="25" duration="150"/>
+   <frame tileid="58" duration="150"/>
+   <frame tileid="91" duration="150"/>
+   <frame tileid="124" duration="150"/>
+   <frame tileid="157" duration="150"/>
+   <frame tileid="190" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="26">
+  <animation>
+   <frame tileid="26" duration="150"/>
+   <frame tileid="59" duration="150"/>
+   <frame tileid="92" duration="150"/>
+   <frame tileid="125" duration="150"/>
+   <frame tileid="158" duration="150"/>
+   <frame tileid="191" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="27">
+  <animation>
+   <frame tileid="27" duration="150"/>
+   <frame tileid="60" duration="150"/>
+   <frame tileid="93" duration="150"/>
+   <frame tileid="126" duration="150"/>
+   <frame tileid="159" duration="150"/>
+   <frame tileid="192" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="28">
+  <animation>
+   <frame tileid="28" duration="150"/>
+   <frame tileid="61" duration="150"/>
+   <frame tileid="94" duration="150"/>
+   <frame tileid="127" duration="150"/>
+   <frame tileid="160" duration="150"/>
+   <frame tileid="193" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="29">
+  <animation>
+   <frame tileid="29" duration="150"/>
+   <frame tileid="62" duration="150"/>
+   <frame tileid="95" duration="150"/>
+   <frame tileid="128" duration="150"/>
+   <frame tileid="161" duration="150"/>
+   <frame tileid="194" duration="150"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ tiled/BossArena/tileSets/objects.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="objects" tilewidth="16" tileheight="16" tilecount="216" columns="24">
+ <image source="../craftpix-dungeon/Tiled_files/Objects.png" width="384" height="144"/>
+ <tile id="55">
+  <animation>
+   <frame tileid="32" duration="380"/>
+   <frame tileid="8" duration="380"/>
+   <frame tileid="8" duration="380"/>
+   <frame tileid="32" duration="380"/>
+   <frame tileid="55" duration="400"/>
+  </animation>
+ </tile>
+</tileset>
